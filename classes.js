4dbfed55d0eaa7401aed3b071b7f2ad531f942c0
John Langdon <johnlangdon@Johns-MacBook-Pro.local>
2020-01-19 12:18:01 -0600

    commit repo

--- classes.js
+++ classes.js
@@ -29,7 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+makeWidget() {
+  return this.first_name + " " + this.last_name + " " + "Widget"
+}
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +57,14 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age)
+    this.reports = [];
+  }
+hire=(employee) => {this.reports.push(employee)}
+fire=(index) => {this.reports.splice(index,1)}
+}
 
 
 ////////// PROBLEM 3 //////////
